19e36624399f11c8416eec99e77f17b2510e9f6f
Pierre Rudloff <contact@rudloff.pro>
2016-11-08 00:23:46 +0100

    Extract in same repository

--- tasks/ar.js
+++ tasks/ar.js
@@ -1,11 +1,19 @@
 /*jslint node: true */
 module.exports = function (grunt) {
     'use strict';
-    var process = require('child_process');
+    var process = require('child_process'),
+        path = require('path');
     grunt.registerMultiTask('ar', 'Extract archive files with GNU ar', function () {
         this.files.forEach(function (f) {
             f.src.forEach(function (src) {
-                process.spawn('ar', ['x', src]);
+                var absoluteSrc = path.resolve(src);
+                process.spawn(
+                    'ar',
+                    ['x', absoluteSrc],
+                    {
+                        cwd: path.dirname(absoluteSrc)
+                    }
+                );
                 grunt.log.writeln('File "' + src + '" extracted');
             });
         });
